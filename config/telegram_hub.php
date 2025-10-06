7bff4221da7ff9205142e63e351f683ff2fe47ba
amirkateb <amveks43@gmail.com>
2025-10-07 00:23:43 +0330

    Create telegram_hub.php

---
+++ config/telegram_hub.php
@@ -0,0 +1,26 @@
+<?php
+
+return [
+    'default_bot' => env('TELEGRAM_DEFAULT_BOT', 'default'),
+    'bots' => [
+        'default' => env('TELEGRAM_BOT_TOKEN'),
+    ],
+    'proxy' => [
+        'enabled' => (bool) env('TELEGRAM_PROXY_ENABLED', false),
+        'http' => env('TELEGRAM_PROXY_HTTP'),
+        'https' => env('TELEGRAM_PROXY_HTTPS'),
+    ],
+    'request' => [
+        'timeout' => (int) env('TELEGRAM_HTTP_TIMEOUT', 15),
+        'connect_timeout' => (int) env('TELEGRAM_HTTP_CONNECT_TIMEOUT', 10),
+    ],
+    'webhook' => [
+        'base_url' => env('TELEGRAM_WEBHOOK_BASE_URL'),
+        'secret_token' => env('TELEGRAM_WEBHOOK_SECRET'),
+    ],
+    'routes' => [
+        'prefix' => env('TELEGRAM_HUB_ROUTE_PREFIX', 'telegram-hub'),
+        'middleware' => explode(',', env('TELEGRAM_HUB_ROUTE_MIDDLEWARE', 'api')),
+    ],
+    'log_channel' => env('TELEGRAM_HUB_LOG_CHANNEL', 'stack'),
+];
