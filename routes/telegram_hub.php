dfdba40278f8e3fd2761414237c4cdeea1c0d67f
amirkateb <amveks43@gmail.com>
2025-10-07 01:15:42 +0330

    Update telegram_hub.php

--- routes/telegram_hub.php
+++ routes/telegram_hub.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use Amirkateb\TelegramHub\Http\Controllers\WebhookController;
+use Amirkateb\TelegramHub\Http\Controllers\WebhookSetupController;
 
 $prefix = config('telegram_hub.routes.prefix', 'telegram-hub');
 $middleware = config('telegram_hub.routes.middleware', ['api']);
@@ -9,6 +10,18 @@ $middleware = config('telegram_hub.routes.middleware', ['api']);
 Route::middleware($middleware)
     ->prefix($prefix)
     ->group(function () {
-        Route::post('/webhook/{bot}', [WebhookController::class, 'handle'])->name('telegram_hub.webhook.handle');
-        Route::post('/webhook', [WebhookController::class, 'handleDefault'])->name('telegram_hub.webhook.handle_default');
+        Route::post('/webhook/{bot}', [WebhookController::class, 'handle'])
+            ->name('telegram_hub.webhook.handle');
+
+        Route::post('/webhook', [WebhookController::class, 'handleDefault'])
+            ->name('telegram_hub.webhook.handle_default');
+
+        Route::post('/set-webhook/{bot}', [WebhookSetupController::class, 'set'])
+            ->name('telegram_hub.webhook.set');
+
+        Route::delete('/delete-webhook/{bot}', [WebhookSetupController::class, 'delete'])
+            ->name('telegram_hub.webhook.delete');
+
+        Route::get('/webhook-info/{bot}', [WebhookSetupController::class, 'info'])
+            ->name('telegram_hub.webhook.info');
     });
