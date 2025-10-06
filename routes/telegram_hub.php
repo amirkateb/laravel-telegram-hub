55fd76829c8207e1dbaeb46ff75397324aa9bce8
amirkateb <amveks43@gmail.com>
2025-10-07 00:26:02 +0330

    Create telegram_hub.php

---
+++ routes/telegram_hub.php
@@ -0,0 +1,15 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use Amirkateb\TelegramHub\Http\Controllers\WebhookController;
+
+$prefix = config('telegram_hub.routes.prefix', 'telegram-hub');
+$middleware = config('telegram_hub.routes.middleware', ['api']);
+
+Route::group([
+    'prefix' => $prefix,
+    'middleware' => $middleware,
+], function () {
+    Route::post('/webhook/{bot}', [WebhookController::class, 'handle'])->name('telegram_hub.webhook.handle');
+    Route::post('/webhook', [WebhookController::class, 'handleDefault'])->name('telegram_hub.webhook.handle_default');
+});
